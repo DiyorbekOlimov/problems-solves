20c78f5a2a656d4086999e78edffd13421bc98f4
Diyorbek <d.olimov04@mail.ru>
2021-09-04 19:54:59 +0500

    leetcode 13

---
+++ roman-to-integer.py
@@ -0,0 +1,9 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        nums = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        out = 0
+        for i, c in enumerate(s):
+            if len(s) == i+1: out += nums[c]
+            elif nums[c] >= nums[s[i+1]]: out += nums[c]
+            else: out -= nums[c]
+        return out
